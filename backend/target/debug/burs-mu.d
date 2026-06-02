054c8884924c54bc17eef78fa868a65825d7e1ac
faruk <faruk.omer.erdem@proton.me>
2026-06-02 23:11:57 +0300

    init

---
+++ backend/target/debug/burs-mu.d
@@ -0,0 +1,1 @@
+/Users/faruk/Projeler/mobil-prog/final/backend/target/debug/burs-mu: /Users/faruk/Projeler/mobil-prog/final/backend/src/main.rs
---
+++ backend/target/debug/deps/burs_mu-dde43f8effed6bc2.d
@@ -0,0 +1,5 @@
+/Users/faruk/Projeler/mobil-prog/final/backend/target/debug/deps/burs_mu-dde43f8effed6bc2.d: src/main.rs
+
+/Users/faruk/Projeler/mobil-prog/final/backend/target/debug/deps/burs_mu-dde43f8effed6bc2: src/main.rs
+
+src/main.rs:
