b645744978f13ec72a90f9b2c7e5ff0d61699c43
faruk <faruk.omer.erdem@proton.me>
2026-06-04 14:58:14 +0300

    Fix widget_test import to use app.dart directly

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:burs_mu/main.dart';
+import 'package:burs_mu/app.dart';
 
 void main() {
   testWidgets('App builds without error', (WidgetTester tester) async {
